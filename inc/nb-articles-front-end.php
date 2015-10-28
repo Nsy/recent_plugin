09bae3308ec3fce03b4f88ec7821007f514e0a5e
Nsy <sylvain.noelie@gmail.com>
2015-10-28 11:30:57 +0100

    add styles for date recent article

--- inc/nb-articles-front-end.php
+++ inc/nb-articles-front-end.php
@@ -1,20 +1,37 @@
-<div class="section-header with-icon"><div class="icon"><span class="glyphicon glyphicon-th-list"></span></div><h5 class="text"><?php echo $title ?></h5></div>
-  <p>
-    <?php
 
-    wp_reset_postdata();
+<div id="sidebar-news" class="section-container clearfix" style="padding-bottom: 10px;">
+  <div class="section-header with-icon" style="margin-bottom: 10px;"><div class="icon"><span class="glyphicon glyphicon-th-list"></span></div><h5 class="text"><?php echo $title ?></h5></div>
+  <div class="section-content clearfix">
 
-    $args = array(
-      'orderby' => 'date',
-      'order'   => 'DESC',
-      'posts_per_page'   => $nsy_nb_articles,
-      );
-    $query = new WP_Query($args);
-    ?>
+    <p>
+      <?php
 
-    <ul>
-      <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-      <li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
-    <?php endwhile; ?>
-  </ul>
-</p>
+      wp_reset_postdata();
+
+      $args = array(
+        'orderby' => 'date',
+        'order'   => 'DESC',
+        'posts_per_page'   => $nsy_nb_articles,
+        );
+      $query = new WP_Query($args);
+      ?>
+
+      <ul class="vertical-simple-list w-dates">
+        <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+        <li class="item clearfix">
+
+          <!--<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>-->
+          <div class="item-content" style="margin-left: -5px;">
+            <div class="date">
+              <div class="day"><?php echo get_the_date('d'); ?></div>
+              <div class="month"><?php echo get_the_date("M"); ?></div>
+            </div>
+            <h6 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h6>
+          </div>
+
+        </li>
+      <?php endwhile; ?>
+    </ul>
+  </p>
+</div>
+</div>
